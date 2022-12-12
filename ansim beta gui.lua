aa16578e9045fe5287e25f79be7f5b4036dc4579
pomka237 <118689961+pomka237@users.noreply.github.com>
2022-12-12 21:23:59 +0300

    Update ansim beta gui.lua

--- ansim beta gui.lua
+++ ansim beta gui.lua
@@ -79,7 +79,7 @@ MainS1:addDropdown({
 
 playersList = {}
 for i, plrt in pairs(game.Players:GetPlayers()) do
-    table.insert(playersList, plrt)
+    table.insert(playersList, plrt.DisplayName.." ("..plrt.Name..")")
 end
 
 tptoplayerdd = MainS1:addDropdown({
@@ -167,7 +167,7 @@ MainS1:addToggle({
     toggled = false,
     callback = function(KAura)
         KAura_ = KAura
-        while wait(0.5) and KAura_ do
+        while wait(0.65) and KAura_ do
             KALPlr = game.Players.LocalPlayer
             for _,player in pairs(game.Players:GetPlayers()) do
                 if player ~= KALPlr then
